7674090e94af7f746170d1834ab642d90b32cd87
Nicole Ocholla <nicoleochola@gmail.com>
2019-11-04 14:53:16 +0300

    Added a hover effect on the porfolio images.

--- js/scripts.js
+++ js/scripts.js
@@ -35,5 +35,55 @@ $("#productDevText").click(function(){
 $("#productDevText").click(function() {
   $("#productDevText").hide();
 });
-$()
+
+// Hover Effect 
+
+$(".image1").hover(function() {
+  $("#work1").show();
+});
+$(".image1").mouseleave(function() {
+  $("#work1").hide();
+});
+$(".image2").hover(function() {
+  $("#work2").show();
+});
+$(".image2").mouseleave(function() {
+  $("#work2").hide();
+});
+$(".image3").hover(function() {
+  $("#work3").show();
+});
+$(".image3").mouseleave(function() {
+  $("#work3").hide();
+});
+$(".image4").hover(function() {
+  $("#work4").show();
+});
+$(".image4").mouseleave(function() {
+  $("#work4").hide();
+});
+$(".image5").hover(function() {
+  $("#work5").show();
+});
+$(".image5").mouseleave(function() {
+  $("#work5").hide();
+});
+$(".image6").hover(function() {
+  $("#work6").show();
+});
+$(".image6").mouseleave(function() {
+  $("#work6").hide();
+});
+$(".image7").hover(function() {
+  $("#work7").show();
+});
+$(".image7").mouseleave(function() {
+  $("#work7").hide();
+});
+$(".image8").hover(function() {
+  $("#work8").show();
+});
+$(".image9").mouseleave(function() {
+  $("#work9").hide();
+});
 });
